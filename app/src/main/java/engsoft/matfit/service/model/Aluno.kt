1a71098bec4b36d1bc366f0a74d01d15fe2d4c92
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-08-04 16:52:06 -0300

    Criado a classe Aluno

---
+++ app/src/main/java/engsoft/matfit/service/model/Aluno.kt
@@ -0,0 +1,18 @@
+package com.devmasterteam.tasks.service.model
+
+data class Aluno(
+    //   @SerializedName("cpf")
+    val cpf: String,
+
+    //  @SerializedName("nome")
+    var nome: String,
+
+    //   @SerializedName("esporte")
+    var esporte: String,
+
+    //   @SerializedName("dataPagamento")
+    var dataPagamento: String = "",
+
+    //   @SerializedName("pagamentoAtrasado")
+    var pagamentoAtrasado: Boolean = false,
+)
