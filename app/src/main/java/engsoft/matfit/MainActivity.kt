95af60ea61fdf0c3fddd69e1c103b027edb41d6f
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-08-04 16:48:27 -0300

    Refatorado a MainActivity

--- app/src/main/java/engsoft/matfit/MainActivity.kt
+++ app/src/main/java/engsoft/matfit/MainActivity.kt
@@ -11,37 +11,47 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.ui.NavigationUI
 import engsoft.matfit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
+        setupNavigation()
+    }
+
+    // Responsável por gerenciar a navegação das fragments associadas à main
+    private fun setupNavigation() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_aluno, R.id.nav_funcionario, R.id.nav_equipamento
-            ), drawerLayout
+            setOf(R.id.nav_aluno, R.id.nav_funcionario, R.id.nav_equipamento),
+            drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navView.setNavigationItemSelectedListener {
+            NavigationUI.onNavDestinationSelected(it, navController)
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
