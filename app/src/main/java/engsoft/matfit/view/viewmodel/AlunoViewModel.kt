7dab3a9c753657e13b40418ab44c0f6b5d042c95
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-08-18 14:22:34 -0300

    Alterado e implementado o AlunoViewModel.kt

---
+++ app/src/main/java/engsoft/matfit/view/viewmodel/AlunoViewModel.kt
@@ -0,0 +1,72 @@
+package engsoft.matfit.view.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import engsoft.matfit.model.Aluno
+import engsoft.matfit.model.AlunoRequest
+import engsoft.matfit.model.AlunoResponse
+import engsoft.matfit.model.AtualizarAlunoRequest
+import engsoft.matfit.service.repository.AlunoRepository
+import kotlinx.coroutines.launch
+
+class AlunoViewModel : ViewModel() {
+
+    private val repository = AlunoRepository()
+
+    private val _listarAlunos = MutableLiveData<List<Aluno>>()
+    val listarAlunos: LiveData<List<Aluno>> = _listarAlunos
+
+    private val _cadastro = MutableLiveData<Boolean>()
+    val cadastro: LiveData<Boolean> = _cadastro
+
+    private val _deletar = MutableLiveData<Boolean>()
+    val deletar: LiveData<Boolean> = _deletar
+
+    private val _buscarAluno = MutableLiveData<AlunoResponse?>()
+    val buscarAluno: LiveData<AlunoResponse?> = _buscarAluno
+
+    private val _atualizarAluno = MutableLiveData<AlunoResponse?>()
+    val atualizarAluno: LiveData<AlunoResponse?> = _atualizarAluno
+
+    fun listarAlunos() {
+        viewModelScope.launch {
+            _listarAlunos.postValue(repository.listarAlunos())
+        }
+    }
+
+    fun cadastrarAluno(aluno: AlunoRequest) {
+        viewModelScope.launch {
+            _cadastro.postValue(repository.cadastrarAluno(aluno))
+        }
+    }
+
+    fun deletarAluno(cpf: String) {
+        viewModelScope.launch {
+            _deletar.postValue(repository.deletarAluno(cpf))
+        }
+    }
+
+    fun buscarAluno(cpf: String) {
+        viewModelScope.launch {
+            try {
+                _buscarAluno.postValue(repository.buscarAluno(cpf))
+            } catch (e: Exception) {
+                _buscarAluno.postValue(null)
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun atualizarAluno(cpf: String, aluno: AtualizarAlunoRequest) {
+        viewModelScope.launch {
+            try {
+                _atualizarAluno.postValue(repository.atualizarAluno(cpf, aluno))
+            } catch (e: Exception) {
+                _atualizarAluno.postValue(null)
+                e.printStackTrace()
+            }
+        }
+    }
+}
