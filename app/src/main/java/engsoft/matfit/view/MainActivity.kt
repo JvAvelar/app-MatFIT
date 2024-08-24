423f7dfc0f2d444b0409c6a4819fdd0d6a20c56a
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-08-23 23:48:40 -0300

    Implementado a lógica do botão de sair na MainActivity

--- app/src/main/java/engsoft/matfit/view/MainActivity.kt
+++ app/src/main/java/engsoft/matfit/view/MainActivity.kt
@@ -1,7 +1,10 @@
 package engsoft.matfit.view
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.widget.TextView
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,9 +14,11 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.ui.NavigationUI
 import engsoft.matfit.R
 import engsoft.matfit.databinding.ActivityMainBinding
+import engsoft.matfit.view.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +27,8 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,7 +36,12 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
         setupNavigation()
+
+        viewModel.nomeUsuario()
+        observador()
     }
 
     // Responsável por gerenciar a navegação das fragments associadas à main
@@ -45,8 +57,12 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
         navView.setNavigationItemSelectedListener {
-            NavigationUI.onNavDestinationSelected(it, navController)
-            drawerLayout.closeDrawer(GravityCompat.START)
+            if (it.itemId == R.id.nav_sair)
+                sair()
+            else {
+                NavigationUI.onNavDestinationSelected(it, navController)
+                drawerLayout.closeDrawer(GravityCompat.START)
+            }
             true
         }
     }
@@ -59,5 +75,19 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun sair() {
+        viewModel.sair()
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun observador() {
+        viewModel.nome.observe(this) {
+            val header = binding.navView.getHeaderView(0)
+            header.findViewById<TextView>(R.id.textName).text = "Olá, $it"
+        }
     }
 }
