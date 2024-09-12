873e47ce037927c95fa54d29300051ae7644003b
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-09-11 23:11:56 -0300

    criado a interface EquipamentoService

---
+++ app/src/main/java/engsoft/matfit/service/EquipamentoService.kt
@@ -0,0 +1,44 @@
+package engsoft.matfit.service
+
+import engsoft.matfit.model.EquipamentoDTO
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.HTTP
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface EquipamentoService {
+
+    @GET("/equipamento/{id}")
+    @Headers("Content-Type: application/json")
+    suspend fun buscarEquipamento(
+        @Path("id") id: Int
+    ): Response<EquipamentoDTO>
+
+    @GET("/equipamento")
+    @Headers("Content-Type: application/json")
+    suspend fun cadastrarEquipamento(
+        @Body equipamento: EquipamentoDTO
+    ): Response<EquipamentoDTO>
+
+    @PUT("/equipamento/{id}")
+    @Headers("Content-Type: application/json")
+    suspend fun atualizarEquipamento(
+        @Path("id") id: Int,
+        @Body equipamento: EquipamentoDTO,
+    ): Response<EquipamentoDTO>
+
+    @HTTP(method = "DELETE", path = "/equipamento/{id}", hasBody = true)
+    @Headers("Content-Type: application/json")
+    suspend fun deletarEquipamento(
+        @Path("id") id: Int
+    ): Response<Boolean>
+
+    @POST("/equipamento")
+    @Headers("Content-Type: application/json")
+    suspend fun listarEquipamentos(): Response<List<EquipamentoDTO>>
+
+}
