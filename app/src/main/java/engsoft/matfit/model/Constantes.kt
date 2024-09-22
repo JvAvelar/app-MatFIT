cd0715fddfb9408f9690c380dc7801ee5ee3b858
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-09-22 03:07:25 -0300

    Criado a classe Constantes, responsável por guardar os valores de chaves para serem usadas ao longe do códigio para evitar hardcoed

---
+++ app/src/main/java/engsoft/matfit/model/Constantes.kt
@@ -0,0 +1,25 @@
+package engsoft.matfit.model
+
+class Constantes {
+
+    object Aluno {
+        const val CPF = "cpf"
+        const val NOME = "nome"
+        const val ESPORTE= "esporte"
+        const val DATA_PAGAMENTO = "dataPagamento"
+        const val PAGAMENTO_ATRASADO = "pagamentoAtrasado"
+    }
+
+    object Funcionario {
+        const val CPF = "cpf"
+        const val NOME = "nome"
+        const val FUNCAO = "funcao"
+        const val CARGA_HORARIA = "cargaHoraria"
+    }
+
+    object Equipamento {
+        const val ID = "id"
+        const val NOME = "nome"
+        const val QUANTIDADE = "quantidade"
+    }
+}
